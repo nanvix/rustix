bfd69765c6c24a35838faa08c1b02411d97d33bc
danbugs <danilochiarlone@gmail.com>
2025-07-04 21:56:06 +0000

    tmp
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- src/maybe_polyfill/std/mod.rs
+++ src/maybe_polyfill/std/mod.rs
@@ -20,11 +20,10 @@ pub mod net {
 pub mod os {
     pub mod fd {
         // Change  to use `std::os::fd` when MSRV becomes 1.66 or higher.
-
-        #[cfg(target_os = "wasi")]
+        
+        #[cfg(all(target_os = "wasi", not(target_os = "nanvix")))]
         pub use std::os::fd::{AsFd, AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, OwnedFd, RawFd};
-        #[cfg(target_os = "nanvix")]
-        pub use std::os::fd::{AsFd, AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, OwnedFd, RawFd};
+        
         #[cfg(unix)]
         pub use std::os::unix::io::{
             AsFd, AsRawFd, BorrowedFd, FromRawFd, IntoRawFd, OwnedFd, RawFd,
